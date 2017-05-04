afe974bac6c86cf7c1c8bac878b22b89e263e57c
Andrew Cooper <andrew.cooper3@citrix.com>
2017-05-04 15:19:31 +0100

    x86/pv: Drop unused switch_kernel_stack()
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Wei Liu <wei.liu2@citrix.com>
    Acked-by: Jan Beulich <jbeulich@suse.com>

--- xen/arch/x86/domain.c
+++ xen/arch/x86/domain.c
@@ -1422,8 +1422,6 @@ static void save_segments(struct vcpu *v)
     this_cpu(dirty_segment_mask) = dirty_segment_mask;
 }
 
-#define switch_kernel_stack(v) ((void)0)
-
 void paravirt_ctxt_switch_from(struct vcpu *v)
 {
     save_segments(v);
@@ -1441,8 +1439,6 @@ void paravirt_ctxt_switch_from(struct vcpu *v)
 void paravirt_ctxt_switch_to(struct vcpu *v)
 {
     unsigned long cr4;
-
-    switch_kernel_stack(v);
 
     cr4 = pv_guest_cr4_to_real_cr4(v);
     if ( unlikely(cr4 != read_cr4()) )
