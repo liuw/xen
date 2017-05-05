ac2a9482e3e5a018cae7f2b7b719e5737747d783
Wei Liu <wei.liu2@citrix.com>
2017-05-05 14:28:03 +0100

    x86/traps: move {un,}register_guest_nmi_callback
    No functional change.
    
    Signed-off-by: Wei Liu <wei.liu2@citrix.com>

--- xen/arch/x86/pv/traps.c
+++ xen/arch/x86/pv/traps.c
@@ -28,6 +28,7 @@
 
 #include <asm/apic.h>
 #include <asm/debugreg.h>
+#include <asm/nmi.h>
 #include <asm/shared.h>
 #include <asm/traps.h>
 
@@ -216,6 +217,42 @@ int set_guest_nmi_trapbounce(void)
     pv_inject_trap(TRAP_nmi, guest_cpu_user_regs());
     tb->flags &= ~TBF_EXCEPTION; /* not needed for NMI delivery path */
     return !null_trap_bounce(v, tb);
+}
+
+long register_guest_nmi_callback(unsigned long address)
+{
+    struct vcpu *v = current;
+    struct domain *d = v->domain;
+    struct trap_info *t = &v->arch.pv_vcpu.trap_ctxt[TRAP_nmi];
+
+    if ( !is_canonical_address(address) )
+        return -EINVAL;
+
+    t->vector  = TRAP_nmi;
+    t->flags   = 0;
+    t->cs      = (is_pv_32bit_domain(d) ?
+                  FLAT_COMPAT_KERNEL_CS : FLAT_KERNEL_CS);
+    t->address = address;
+    TI_SET_IF(t, 1);
+
+    /*
+     * If no handler was registered we can 'lose the NMI edge'. Re-assert it
+     * now.
+     */
+    if ( (v->vcpu_id == 0) && (arch_get_nmi_reason(d) != 0) )
+        v->nmi_pending = 1;
+
+    return 0;
+}
+
+long unregister_guest_nmi_callback(void)
+{
+    struct vcpu *v = current;
+    struct trap_info *t = &v->arch.pv_vcpu.trap_ctxt[TRAP_nmi];
+
+    memset(t, 0, sizeof(*t));
+
+    return 0;
 }
 
 /*
