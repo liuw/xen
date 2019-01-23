749c07d57f3b22b4a8aca2a274344f9d03449cb2
Wei Liu <wei.liu2@citrix.com>
2019-01-23 15:33:07 +0000

    x86: introduce a new set of APIs to manage Xen page tables
    We are going to switch to using domheap page for page tables.
    A new set of APIs is introduced to allocate, map, unmap and free pages
    for page tables.
    
    The allocation and deallocation work on mfn_t but not page_info,
    because they are required to work even before frame table is set up.
    
    Implement the old functions with the new ones. We will rewrite, patch
    by patch, other mm functions that manipulate page tables to use the
    new APIs.
    
    Note these new APIs still use xenheap page underneath and no actual
    map and unmap is done so that we don't break xen half way. They will
    be switched to use domheap and dynamic mappings.
    
    No functional change intended in this patch.
    
    Signed-off-by: Wei Liu <wei.liu2@citrix.com>

--- xen/include/asm-x86/mm.h
+++ xen/include/asm-x86/mm.h
@@ -644,6 +644,10 @@ int arch_acquire_resource(struct domain *d, unsigned int type,
 /* Allocator functions for Xen pagetables. */
 void *alloc_xen_pagetable(void);
 void free_xen_pagetable(void *v);
+mfn_t alloc_xen_pagetable_new(void);
+void *map_xen_pagetable_new(mfn_t mfn);
+void unmap_xen_pagetable_new(void *v);
+void free_xen_pagetable_new(mfn_t mfn);
 l1_pgentry_t *virt_to_xen_l1e(unsigned long v);
 
 #endif /* __ASM_X86_MM_H__ */
