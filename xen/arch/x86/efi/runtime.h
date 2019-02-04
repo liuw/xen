d67951247a3520430b0444c891ef27dc280f4507
Wei Liu <wei.liu2@citrix.com>
2019-02-04 17:19:27 +0000

    efi: switch EFI L4 table to use new APIs
    This requires storing the MFN instead of linear address of the L4
    table. Adjust code accordingly.
    
    Signed-off-by: Wei Liu <wei.liu2@citrix.com>

--- xen/arch/x86/efi/runtime.h
+++ xen/arch/x86/efi/runtime.h
@@ -2,11 +2,17 @@
 #include <asm/mc146818rtc.h>
 
 #ifndef COMPAT
-l4_pgentry_t *__read_mostly efi_l4_pgtable;
+mfn_t __read_mostly efi_l4_mfn = INVALID_MFN_INITIALIZER;
 
 void efi_update_l4_pgtable(unsigned int l4idx, l4_pgentry_t l4e)
 {
-    if ( efi_l4_pgtable )
-        l4e_write(efi_l4_pgtable + l4idx, l4e);
+    if ( !mfn_eq(efi_l4_mfn, INVALID_MFN) )
+    {
+        l4_pgentry_t *l4t;
+
+        l4t = map_xen_pagetable_new(efi_l4_mfn);
+        l4e_write(l4t + l4idx, l4e);
+        UNMAP_XEN_PAGETABLE_NEW(l4t);
+    }
 }
 #endif
